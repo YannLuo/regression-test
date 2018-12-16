cec610fc7deccdc82dda31038a4607d885b6671c
YannLuo <yannluo@foxmail.com>
2018-12-16 20:11:19 +0800

    181216

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     #
     # repo_path = os.path.join('ecosystem', '**', '*.py')
     # stt = time.clock()
-    # graph = dump_callgraph(repo_path, logger=logger)
+    # callgraph = dump_callgraph(repo_path, logger=logger)
     # edt = time.clock()
     # print("Generate callgraph cost %.2f minutes." % ((edt - stt) / 60.0, ))
 
@@ -24,15 +24,14 @@ def main():
 
     repo_path = os.path.join('REPOS', 'numpy')
     repo = git.Repo(repo_path)
-    commit_sha = "5e6ff3f"
-    pre_commit_sha = f"{commit_sha}~5"
+    commit_sha = "7a3c50a"
+    pre_commit_sha = f"{commit_sha}~1"
     diff_content = repo.git.diff(pre_commit_sha, commit_sha)
     diff_infos = parse_diff(diff_content)
 
     mod_functiondef_list = set()
 
     for diff in diff_infos:
-
         try:
             repo.git.checkout(['-f', commit_sha])
         except:
@@ -69,26 +68,59 @@ def main():
 
     # ========== calculate reverse callgraph ==========
 
-    with open("callgraph.json", mode='r', encoding='utf-8') as rf:
-        callgraph = json.load(rf)
+    # with open("callgraph.json", mode='r', encoding='utf-8') as rf:
+    #     callgraph = json.load(rf)
+
+    callgraph = defaultdict(set)
+    with open("astropy_callgraph.txt", mode="r", encoding="utf-8") as rf:
+        lines = rf.readlines()
+    for line in lines:
+        src, dst = line.strip().split()
+        if (src.startswith("numpy") or src.startswith("astropy")) and (dst.startswith("numpy") or dst.startswith("astropy")):
+            callgraph[src].add(dst)
+
+    test_files = set()
+    for call_site in callgraph:
+        if ".tests." in call_site and call_site.startswith('astropy'):
+            spl_file = []
+            spl_cs = call_site.split('.')[:-1]
+            for ssi in spl_cs:
+                if ssi[0].isupper():
+                    break
+                spl_file.append(ssi)
+            file = '.'.join(spl_file)
+            test_files.add(file)
+    print(len(test_files))
 
     rev_callgraph = defaultdict(set)
     for caller, callee_list in callgraph.items():
         for callee in callee_list:
-            rev_callgraph["%s %s %s" % (callee["namespace"], callee["name"], callee["flavor"])].add(caller)
+            rev_callgraph[callee].add(caller)
+
+    for k in rev_callgraph:
+        rev_callgraph[k] = list(rev_callgraph[k])
+
+    with open('rev_callgraph.json', mode='w', encoding='utf-8') as wf:
+        wf.write(
+            json.dumps(rev_callgraph,
+                       default=lambda o: o.__dict__,
+                       indent=4)
+        )
 
     # ========== analyze change impact ==========
 
     s = set()
+    q = []
     for prefix_namespace, name in mod_functiondef_list:
-        q = []
         for cur_call in rev_callgraph:
-            if cur_call.startswith(prefix_namespace) and cur_call.split(' ')[1] == name and cur_call not in s:
+            if cur_call.startswith(prefix_namespace) and cur_call.split('.')[-1] == name and cur_call not in s:
                 if cur_call not in s:
                     q.append(cur_call)
                     s.add(cur_call)
+
     while len(q):
         top = q[0]
+        # print(top)
         q = q[1:]
         if top in rev_callgraph:
             for si in rev_callgraph[top]:
@@ -98,11 +130,15 @@ def main():
 
     selected_tests_module = set()
     for si in s:
-        if ".tests." in si:
-            namespace, name, flavor = si.split(" ")
-            if flavor != "Flavor.FUNCTION" and namespace.rfind(".") != -1:
-                namespace = namespace[:namespace.rfind(".")]
-            selected_tests_module.add(namespace)
+        if ".tests." in si and si.startswith('astropy'):
+            spl_file = []
+            spl_si = si.split('.')[:-1]
+            for ssi in spl_si:
+                if ssi[0].isupper():
+                    break
+                spl_file.append(ssi)
+            file = '.'.join(spl_file)
+            selected_tests_module.add(file)
 
     for item in selected_tests_module:
         print(item)

4c1b740649b6fc29336fdee230bd788946953d41
YannLuo <yannluo@foxmail.com>
2019-03-25 11:28:07 +0800

    190325

--- dump_callgraph.py
+++ dump_callgraph.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 REPOS = ["astropy", "ccdproc", "dask", "gammapy", "h5py", "IPython", "joblib", "matplotlib", "nbconvert",
          "networkx", "nilearn", "numba", "numexpr", "numpy", "obspy", "pandas", "scipy", "seaborn", "skbio", "sklearn",
          "specutils", "statsmodels", "sympy", "tables", "theano", "xarray", "photutils", "asdf", "poppy", "astropy_helpers",
-         "bottleneck", "pyregion", "brian2", "naima", "pyamg", "patsy", "astroplan"][-1:]
+         "bottleneck", "pyregion", "brian2", "naima", "pyamg", "patsy", "astroplan", "radio_beam"][-1:]
 UPSTREAM_DICT = {
     "astropy": "scipy",
     "ccdproc": "scipy",
@@ -44,7 +44,8 @@ UPSTREAM_DICT = {
     "naima": "scipy",
     "pyamg": "scipy",
     "patsy": "numpy",
-    "astroplan": "numpy"
+    "astroplan": "numpy",
+    "radio_beam": "numpy"
 }
 
 
